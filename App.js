0a3d88813f6fec9cb9688b73c367311f746eeb42
agnel <agnelrozario@ymail.com>
2024-06-13 18:12:34 +0400

    new
    new

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import exercises from './assets/data/exercises.json';
+import ExerciseListItem from './components/ExcerciseListItem'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={exercises}
+        contentContainerStyle={{ gap: 5 }}
+        renderItem={({ item }) => <ExerciseListItem item={item} />}
+      />
     </View>
   );
 }
@@ -13,8 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#f3f3f3',
+    padding: 10,
     justifyContent: 'center',
+    paddingTop: 70
   },
 });
